9aa5982e4b24b66740650d6d8ecd6a2ae00627d8
websorokin <bohdansorokin7@gmail.com>
2022-02-04 08:01:49 +0200

    Changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,22 @@ npm run build
 
 cd dist
 
-git init
-git add -A
-git commit -m 'New deployment'
+PROJECT="D:/Workplace/11/launchbar"
+DIST="D:/Workplace/11/launchbar/dist"
+
+cp -f "$DIST/index.html" $PROJECT
+cp -f -r "$DIST/js" $PROJECT
+cp -f -r "$DIST/css" $PROJECT
+cp -f -r "$DIST/images" $PROJECT
+cp -f -r "$DIST/videos" $PROJECT
+
+git add index.html
+git add css
+git add js
+git add images
+git add videos
+
+git commit -m 'Changes'
 
 git push -f git@github.com:websorokinweb/LaunchBar.git main:gh-pages
 
